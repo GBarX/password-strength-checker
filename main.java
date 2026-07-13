d1480121f1a56d66fdb03cc7445120de518ba5c4
Berk Göksu <goksuberk34@gmail.com>
2026-07-13 12:24:06 +0300

    feat: add interactive menu to Main #1

--- main.java
+++ main.java
@@ -1,27 +1,69 @@
 import java.io.IOException;
 
 import evaluator.PasswordEvaluator;
+import generator.GeneratorService;
+import generator.PasswordGenerator;
+import generator.RandomPasswordGenerator;
 import result.EvaluationResult;
+import result.GenerationResult;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
         PasswordEvaluator evaluator = new PasswordEvaluator();
 
-        String password = "test1234!";
-        String password2 = "testlkwq1";
-        String password3 = "qwer125678";
-        String password4 = "1kasd1!DJamsiqkals!?AQsnzjQ@kau";
-        String password5 = "12KAShax";
-        String password6 = "QAZ123ZAQ";
+        System.out.println("Choose an option:");
+        System.out.println("1 - Test your password strength!");
+        System.out.println("2 - Generate strong password");
+        System.out.println("3 - Generate Passphrase");
+        int choice = sc.nextInt();
+        sc.nextLine();
 
-        EvaluationResult result = evaluator.evaluate(password3);
+        GeneratorService service = new GeneratorService(evaluator, 60);
 
-        System.out.println("Level: " + result.getLevel());
-        System.out.println("Entropy: " + result.getEntropy() + " bits");
-        System.out.println("Crack time: " + result.getCrackTime());
-        System.out.println("Feedback:");
-        for (String msg : result.getMessages()) {
-            System.out.println(" - " + msg);
+        switch (choice) {
+            case 1:
+                System.out.println("Enter your password to see your password's strength: ");
+                String password = sc.nextLine();
+                EvaluationResult result = evaluator.evaluate(password);
+
+                System.out.println("Level: " + result.getLevel());
+                System.out.println("Entropy: " + result.getEntropy() + " bits");
+                System.out.println("Crack time: " + result.getCrackTime());
+                System.out.println("Feedback:");
+                for (String msg : result.getMessages()) {
+                    System.out.println(" - " + msg);
+                }
+                break;
+            case 2:
+                System.out.println("Enter your password length: ");
+                int length = sc.nextInt();
+                System.out.println("Upper case? (True/False): ");
+                boolean upperCase = sc.nextBoolean();
+                System.out.println("Lower case? (True/False): ");
+                boolean lowerCase = sc.nextBoolean();
+                System.out.println("Digits? (True/False): ");
+                boolean digits = sc.nextBoolean();
+                System.out.println("Symbols? (True/False): ");
+                boolean symbols = sc.nextBoolean();
+
+                RandomPasswordGenerator passGen = new RandomPasswordGenerator(length, upperCase, lowerCase, digits,
+                        symbols);
+                service.setGenerator(passGen);
+
+                GenerationResult gr = service.generateValidated();
+                System.out.println("Password: " + gr.getPassword());
+                System.out.println("Level: " + gr.getEvaluation().getLevel());
+                System.out.println("Entropy: " + gr.getEvaluation().getEntropy());
+                break;
+            default:
+                System.out.println("Invalid input!");
+                break;
         }
+
+        sc.close();
     }
 }
